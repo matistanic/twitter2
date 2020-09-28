edb810d226f45355d5ac02d8d13c1d20fdc938b4
Matías <matias.mullerg@mail.udp.cl>
2020-09-28 00:19:41 -0300

    last commit please

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image_url)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image_url]
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sitizer.permit(:account_update, keys: [:name, :image_url)
+    devise_parameter_sitizer.permit(:account_update, keys: [:name, :image_url]
   end
 
   # The path used after sign up.
